6d322e2b51ef5ab39a96aaf920ba52d3aff161f7
Shobith R Acharya <96403075+Shobith16@users.noreply.github.com>
2026-04-11 23:37:33 +0530

    SRA-11/04/2026

--- java/inttoroman.java
+++ java/inttoroman.java
@@ -1,8 +1,8 @@
 class Solution {
     public static String intToRoman(int num) {
         // All values including subtractive forms, in descending order
-        int[] values       = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] symbols   = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        String[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
 
         StringBuilder result = new StringBuilder();
 
@@ -14,7 +14,7 @@ class Solution {
             }
         }
 
-        return result.toString(); 
+        return result.toString();
     }
 
     public static void main(String[] args) {
