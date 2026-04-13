a9828c92f8f20e471953fdfe642c8ddc5402dc28
Shobith R Acharya <96403075+Shobith16@users.noreply.github.com>
2026-04-13 22:09:01 +0530

    SRA-13/04/2026-01

---
+++ sql/sql.sql
@@ -0,0 +1,7 @@
+SELECT firstname, lastname , city, state FROM person LEFT JOIN address  ON person.personid = address.personid
+
+--second-highest-salary
+select coalesce((select salary  from (select salary,Dense_Rank() Over 
+(order by salary  Desc) as rnk  FROM employee) as ranked where rnk = 2 Limit 1),NuLL)as SecondHighestSalary
+
+--
