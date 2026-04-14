7dbf777d4f09e4761ee6dea2cfe4bc3e121f513d
Shobith R Acharya <96403075+Shobith16@users.noreply.github.com>
2026-04-14 22:54:01 +0530

    SRA-14/04/2026-01

--- java/test.java
+++ java/test.java
@@ -9,8 +9,23 @@ class test {
         }
         return -1;
     }
+
+    public static boolean isPalindrome(String s) {
+        // Remove all spaces
+        s = s.replaceAll("\\s", "");
+
+        // Remove special characters (keep only alphanumeric)
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        return s.equals(new StringBuilder(s).reverse().toString().toLowerCase());
+           
+    }
     public static void main(String[] args) {
         //first-occurrence-in-a-string
         System.out.println("Res : " + strStr("sadbutsad", "sad"));
+        System.out.println("Res : " + strStr("leetcode", "leeto"));
+        //palindrome
+        System.out.println("Res : " + isPalindrome("A man a plan a canal Panama"));
+        System.out.println("Res : " + isPalindrome("race a car"));
     }
 }
